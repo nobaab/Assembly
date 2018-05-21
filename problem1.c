e12accc15e203512c2886881815c8e68940b3526
Mozammel Hoshen Chowdhury <36574324+nobaab@users.noreply.github.com>
2018-05-22 00:48:50 +0600

    Update problem1.c

--- problem1.c
+++ problem1.c
@@ -1,21 +1,23 @@
 #include <stdio.h>
 
-void rev(int i, int n, int a[10])
+void rev(int i, int n, int arr[10])
 {
     if(i<n)
     {
-        rev(i+1, n, a);
-        printf("%d ", a[i]);
+        rev(i+1, n, arr);
+        printf("%d ", arr[i]);
     }
 }
 
 int main()
 {
-    int i, n, a[10];
+    int i, n, arr[10];
     scanf("%d", &n);
-    for(i=0; i<n; i++)
-        scanf("%d", &a[i]);
-    rev(0, n, a);
+    for(i=0; i<n; i++){
+    scanf("%d", &arr[i]);
+    }
+    rev(0, n, arr);
     printf("\n");
+    
     return 0;
 }
